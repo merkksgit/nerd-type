43221b5a3877fc8f20153312e963383b928437ba
merkksgit <merkks@protonmail.com>
2024-10-07 01:32:35 +0300

    nightmare mode added

---
+++ js/functions-nm.js
@@ -0,0 +1,65 @@
+let timeLeft = 6; // Aloitusaika sekunteina
+let totalTimeSpent = 0;
+let currentWordIndex = 0;
+let countDownInterval;
+let totalTimeInterval;
+import { words } from "./words-nm.js";
+console.log(words);
+
+const resetBtn = document.getElementById("resetBtn"); // Hae reset-painike
+
+document.getElementById("startButton").addEventListener("click", startGame);
+
+function startGame() {
+  currentWordIndex = Math.floor(Math.random() * words.length); // Valitse satunnainen indeksi
+  document.getElementById("wordToType").textContent = words[currentWordIndex];
+  updateTimer();
+  countDownInterval = setInterval(countDown, 1000);
+  totalTimeInterval = setInterval(totalTimeCount, 1000);
+  document.getElementById("userInput").focus();
+}
+
+// aloittaa pelin painamalla "Enter"
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Enter") {
+    startGame();
+  }
+});
+
+resetBtn.addEventListener("click", () => {
+  location.reload(); // Lataa sivu uudelleen
+});
+
+function countDown() {
+  if (timeLeft > 0) {
+    timeLeft--;
+    updateTimer();
+  } else {
+    clearInterval(countDownInterval);
+    clearInterval(totalTimeInterval);
+    alert("Game Over");
+    location.reload(); // Pelin uudelleenkäynnistys
+  }
+}
+
+function totalTimeCount() {
+  totalTimeSpent++;
+  document.getElementById("totalTimeSpent").textContent =
+    totalTimeSpent + " seconds";
+}
+
+function updateTimer() {
+  document.getElementById("timeLeft").textContent = timeLeft + " seconds";
+}
+
+document.getElementById("userInput").addEventListener("input", checkInput);
+
+function checkInput() {
+  const userInput = document.getElementById("userInput").value;
+  if (userInput === words[currentWordIndex]) {
+    timeLeft += 2; // Lisää x sekuntia aikaa
+    currentWordIndex = Math.floor(Math.random() * words.length); // Valitse uusi satunnainen sana
+    document.getElementById("wordToType").textContent = words[currentWordIndex];
+    document.getElementById("userInput").value = ""; // Tyhjennä syötekenttä
+  }
+}
