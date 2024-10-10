727d5e710281ba261509ca7d21b2e40b1d651176
merkksgit <merkks@protonmail.com>
2024-10-11 01:43:26 +0300

    progress bar added

--- js/functions-eng.js
+++ js/functions-eng.js
@@ -38,20 +38,23 @@ function countDown() {
     // Peli päättyy, kun timeLeft menee nollaan
     clearInterval(countDownInterval);
     clearInterval(totalTimeInterval);
-    showGameOverModal("You need a snack,"); // Näytä eri viesti
+    showGameOverModal("Hack FAILED! You need a snack,"); // Näytä eri viesti
   }
 }
 
 function totalTimeCount() {
   totalTimeSpent++;
-  document.getElementById("totalTimeSpent").textContent =
-    totalTimeSpent + " seconds";
+  // poistetaan näytöltä kokonaisaika kommentoimalla tämä
+  // document.getElementById("totalTimeSpent").textContent =
+  //   totalTimeSpent + " seconds";
 
-  if (totalTimeSpent >= 90) {
-    // Peli päättyy, kun kokonaisaika on x sekuntia
+  updateProgressBar(); // Päivitä progress bar
+
+  if (totalTimeSpent >= 60) {
+    // Peli päättyy, kun kokonaisaika on 90 sekuntia
     clearInterval(countDownInterval);
     clearInterval(totalTimeInterval);
-    showGameOverModal("Wau! You just hacked the Pentagon!"); // Näytä eri viesti
+    showGameOverModal("Pentagon HACKED! All that work and still"); // Näytä eri viesti
   }
 }
 
@@ -59,12 +62,33 @@ function updateTimer() {
   document.getElementById("timeLeft").textContent = timeLeft;
 }
 
+function updateProgressBar() {
+  // Laske edistyminen prosentteina
+  const progressPercentage = (totalTimeSpent / 60) * 100;
+
+  // Päivitä progress barin leveys
+  const progressBar = document.getElementById("progressBar");
+  progressBar.style.width = progressPercentage + "%";
+  progressBar.setAttribute("aria-valuenow", progressPercentage);
+
+  // Change color dynamically based on the progress
+  if (progressPercentage < 80) {
+    progressBar.style.backgroundColor = "#7aa2f7"; // Blue
+  } else {
+    progressBar.style.backgroundColor = "#ff007c"; // Red
+  }
+
+  // Päivitä prosenttiluku
+  document.getElementById("progressPercentage").textContent =
+    "Hacked " + Math.floor(progressPercentage) + "%";
+}
+
 document.getElementById("userInput").addEventListener("input", checkInput);
 
 function checkInput() {
   const userInput = document.getElementById("userInput").value;
   if (userInput === words[currentWordIndex]) {
-    timeLeft += 3; // Lisää x sekuntia aikaa
+    timeLeft += 3; // Lisää 3 sekuntia aikaa
     currentWordIndex = Math.floor(Math.random() * words.length); // Valitse uusi satunnainen sana
     document.getElementById("wordToType").textContent = words[currentWordIndex];
     document.getElementById("userInput").value = ""; // Tyhjennä syötekenttä
@@ -77,9 +101,9 @@ function showGameOverModal(message) {
   document.getElementById("gameOverModalLabel").textContent = "Game Over";
   document.querySelector(".modal-body").innerHTML =
     message +
-    "<br />You have " +
+    "<br />you have " +
     timeLeft +
-    " energy left.<br />Well played nerd!";
+    " energy left.<br />Try another hack by pressing 'Return'.";
 
   // Näytä Bootstrap-modaali
   let gameOverModal = new bootstrap.Modal(
