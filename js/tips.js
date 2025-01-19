5993643effd41c8c5c2ccdbe03d20748e67c43ba
merkksgit <merkks@protonmail.com>
2025-01-19 22:17:54 +0200

    modified:   js/tips.js

--- js/tips.js
+++ js/tips.js
@@ -1,10 +1,10 @@
 export const tips = [
   "Enter (Start), Ctrl + Enter (Reset)",
   "Type 'terminal' to open the terminal window",
-  "There is no 'God Mode'",
-  "Focus on accuracy over speed",
-  "Place your fingers on the 'home row' keys",
-  "Practice typing without looking at the keyboard",
-  "Terminal: Use TAB key to auto-complete commands and filenames",
-  "You can modify the game difficulty in the terminal",
+  // "There is no 'God Mode'",
+  // "Focus on accuracy over speed",
+  // "Place your fingers on the 'home row' keys",
+  // "Practice typing without looking at the keyboard",
+  // "Terminal: Use TAB key to auto-complete commands and filenames",
+  // "You can modify the game difficulty in the terminal",
 ];
