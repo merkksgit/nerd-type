b35a37b525169fd9395bc39bc559e10a58f039c3
merkksgit <merkks@protonmail.com>
2025-03-27 22:20:35 +0200

    modified:   js/tips.js

--- js/tips.js
+++ js/tips.js
@@ -6,5 +6,4 @@ export const tips = [
   " You can modify the game difficulty in the terminal ",
   " Use number keys 1-9 to quickly navigate between pages ",
   " Open terminal and type 'help' for available commands ",
-  " Embrace the nerd in you! ",
 ];
