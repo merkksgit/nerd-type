21b6c07cf0851cbb650f4d238ad35daaccafba0c
merkksgit <merkks@protonmail.com>
2025-03-27 18:05:42 +0200

    modified:   js/tips.js

--- js/tips.js
+++ js/tips.js
@@ -1,10 +1,10 @@
 export const tips = [
-  " Enter (Start), Ctrl + Enter (Reset)",
-  " Type 'terminal' to open the terminal window",
-  " There is no God Mode",
-  " Use TAB key to auto-complete commands and filenames",
-  " You can modify the game difficulty in the terminal",
-  " Use number keys 1-9 to quickly navigate between pages",
-  " Open terminal and type 'help' for available commands",
-  " Embrace the nerd in you",
+  " Enter (Start), Ctrl + Enter (Reset) ",
+  " Type 'terminal' to open the terminal window ",
+  " There is no God Mode ",
+  " Use TAB key to auto-complete commands and filenames ",
+  " You can modify the game difficulty in the terminal ",
+  " Use number keys 1-9 to quickly navigate between pages ",
+  " Open terminal and type 'help' for available commands ",
+  " Embrace the nerd in you ",
 ];
