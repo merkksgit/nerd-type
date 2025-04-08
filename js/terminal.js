5fa14ba43c6f5d68fc5cec002da5d192da53a912
merkksgit <merkks@protonmail.com>
2025-04-08 23:00:28 +0300

    modified:   js/terminal.js

--- js/terminal.js
+++ js/terminal.js
@@ -261,7 +261,7 @@ class Terminal {
 ██║ ╚████║███████╗██║  ██║██████╔╝   ██║      ██║   ██║     ███████╗
 ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═╝╚═════╝    ╚═╝      ╚═╝   ╚═╝     ╚══════╝</span>
                                                 
-<span style="color:#bb9af7">Welcome to NerdType Terminal v1.0.1</span>
+<span style="color:#bb9af7">NerdType Terminal v1.0.1</span>
 <span style="color:#7dcfff">Type 'help' for available commands</span>
 `;
     }
