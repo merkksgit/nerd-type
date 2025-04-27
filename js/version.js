4e6cf1f9ffb3375b03719fa9f4765fcd79cf6ea0
merkksgit <merkks@protonmail.com>
2025-04-27 19:46:48 +0300

    v.1.3.8

--- js/version.js
+++ js/version.js
@@ -1,5 +1,5 @@
 // Set the application version in one place
-const APP_VERSION = "NerdType v1.3.7";
+const APP_VERSION = "NerdType v1.3.8";
 
 // When the document is loaded, update all version elements
 document.addEventListener("DOMContentLoaded", function () {
