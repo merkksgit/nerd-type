a992eee3ff3aa6d380d4b1ccb33853f442603661
merkksgit <merkks@protonmail.com>
2025-04-30 17:27:12 +0300

    v1.4.2

--- js/version.js
+++ js/version.js
@@ -1,5 +1,5 @@
 // Set the application version in one place
-const APP_VERSION = "NerdType v1.4.1";
+const APP_VERSION = "NerdType v1.4.2"; // Muista muuttaa versio myös README tiedostoon
 
 // When the document is loaded, update all version elements
 document.addEventListener("DOMContentLoaded", function () {
